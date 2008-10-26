29c2f42b45d01b03a660db067c1430df32733331
Sven Reinck <sreinck@fluxparticle.de>
2008-10-26 11:46:46 +0100

    da war wohl jemand farbenblind

--- Color.cpp
+++ Color.cpp
@@ -21,7 +21,7 @@
 
 Color4 Color4::white(1.0f, 1.0f, 1.0f);
 Color4 Color4::gray(0.5f, 0.5f, 0.5f);
-Color4 Color4::red(0.0f, 0.0f, 1.0f);
+Color4 Color4::red(1.0f, 0.0f, 0.0f);
 Color4 Color4::blue(0.0f, 0.0f, 1.0f);
 Color4 Color4::yellow(1.0f, 1.0f, 0.0f);
 
